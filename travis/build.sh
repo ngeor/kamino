9a1d1e8e2110cb3c129def820ddf85f96304670d
Nikolaos Georgiou <Nikolaos.Georgiou@gmail.com>
2017-03-31 21:27:12 +0200

    Using the major minor version from pom.xml when creating a new version during build (Fixes #8)

--- travis/build.sh
+++ travis/build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-
+set -e # break on error
 echo "Current branch: ${TRAVIS_BRANCH}"
 
 # Import GPG key
 gpg --passphrase=${GPG_PASSPHRASE} --no-use-agent --output - keys.asc | gpg --import
 
 # get existing version from pom
+# first run to get the downloading out of the way
+mvn -q help:evaluate -Dexpression=project.version
 MAJOR_MINOR_VERSION=$(mvn help:evaluate -Dexpression=project.version | grep ^[0-9] | cut -d- -f1 | cut -d. -f1,2)
 
 # define a custom version for the pom
