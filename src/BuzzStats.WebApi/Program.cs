ef7c6126a9768d8bba47c39b0003d05c82a3322b
Nikolaos Georgiou <Nikolaos.Georgiou@gmail.com>
2017-07-01 19:23:04 +0200

    Adding sample webapi project

---
+++ src/BuzzStats.WebApi/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Owin.Hosting;
+
+namespace BuzzStats.WebApi
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            const string baseAddress = "http://localhost:9000/";
+
+            // Start OWIN host 
+            using (WebApp.Start<Startup>(url: baseAddress)) 
+            {
+                Console.WriteLine("Server listening at port 9000");
+                Console.ReadLine(); 
+            }
+        }
+    }
+}
