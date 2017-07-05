15ccb3a657120b56a80ccaccfe3d60c4dcbf9c36
Nikolaos Georgiou <Nikolaos.Georgiou@gmail.com>
2017-07-05 20:30:46 +0200

    Starting a mock server. Corrected dependencies of WebApi project.

---
+++ src/BuzzStats.MockServer/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+namespace BuzzStats.MockServer
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            const string baseAddress = "http://localhost:9900/";
+
+            // Start OWIN host 
+            using (WebApp.Start<Startup>(url: baseAddress)) 
+            {
+                Console.WriteLine("Server listening at port 9900");
+                Console.ReadLine(); 
+            }
+        }
+    }
+    
+    public class Startup
+    {
+        // This code configures Web API. The Startup class is specified as a type
+        // parameter in the WebApp.Start method.
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            appBuilder.Use<MyMiddleware>();
+        }
+    }
+
+    public class MyMiddleware : OwinMiddleware
+    {
+        public MyMiddleware(OwinMiddleware next) : base(next)
+        {
+        }
+
+        public override async Task Invoke(IOwinContext context)
+        {
+            if (!context.Request.Path.Value.Equals("/hello"))
+            {
+                await Next.Invoke(context);
+                return;
+            }
+
+            context.Response.StatusCode = (int) HttpStatusCode.OK;
+            context.Response.ContentType = "text/html";
+            context.Response.Write("<html><body><h1>Test</h1><p>Hi</p></body></html>");
+        }
+    }
+}
--- src/BuzzStats.WebApi/ValuesController.cs
+++ src/BuzzStats.WebApi/ValuesController.cs
@@ -3,33 +3,33 @@ using System.Web.Http;
 
 namespace BuzzStats.WebApi
 {
-    public class ValuesController : ApiController 
-    { 
+    public class ValuesController : ApiController
+    {
         // GET api/values 
-        public IEnumerable<string> Get() 
-        { 
-            return new[] { "value1", "value2" }; 
-        } 
+        public IEnumerable<string> Get()
+        {
+            return new[] {"value1", "value2"};
+        }
 
         // GET api/values/5 
-        public string Get(int id) 
-        { 
-            return "value"; 
-        } 
+        public string Get(int id)
+        {
+            return "value";
+        }
 
         // POST api/values 
-        public void Post([FromBody]string value) 
-        { 
-        } 
+        public void Post([FromBody] string value)
+        {
+        }
 
         // PUT api/values/5 
-        public void Put(int id, [FromBody]string value) 
-        { 
-        } 
+        public void Put(int id, [FromBody] string value)
+        {
+        }
 
         // DELETE api/values/5 
-        public void Delete(int id) 
-        { 
-        } 
-    } 
+        public void Delete(int id)
+        {
+        }
+    }
 }
