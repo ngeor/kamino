c814aa5f4be2949327d8281c93c79ec8bf354924
Nikolaos Georgiou <Nikolaos.Georgiou@gmail.com>
2017-07-08 09:13:25 +0200

    Unit test for Startup class

---
+++ src/BuzzStats.MockServer.UnitTests/StartupTest.cs
@@ -0,0 +1,24 @@
+﻿using Moq;
+using NUnit.Framework;
+using Owin;
+
+namespace BuzzStats.MockServer.UnitTests
+{
+    [TestFixture]
+    public class StartupTest
+    {
+        [Test]
+        public void UsesMiddleware()
+        {
+            // arrange
+            IAppBuilder appBuilder = Mock.Of<IAppBuilder>();
+            Startup startup = new Startup();
+            
+            // act
+            startup.Configuration(appBuilder);
+            
+            // assert
+            Mock.Get(appBuilder).Verify(a => a.Use(typeof(MockBuzzMiddleware)));
+        }
+    }
+}
