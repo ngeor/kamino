649f7cca0f2adcadad18d3c9c687ded998254849
Nikolaos Georgiou <Nikolaos.Georgiou@gmail.com>
2017-07-17 20:58:12 +0200

    Added watch task. Added original markup and css.

--- BuzzStats.WebApi/gulpfile.js
+++ BuzzStats.WebApi/gulpfile.js
@@ -2,9 +2,17 @@ var gulp = require('gulp');
 var pug = require('gulp-pug');
 var less = require('gulp-less');
 
+// TODO read version from package.json
+var version = '2.0.0';
+
 gulp.task('html', function() {
     return gulp.src('templates/*.pug')
-        .pipe(pug({ pretty: true }))
+        .pipe(pug({
+            pretty: true,
+            data: {
+                version: version
+            }
+        }))
         .pipe(gulp.dest('bin/Debug'));
 });
 
@@ -12,6 +20,11 @@ gulp.task('css', function() {
     return gulp.src('styles/*.less')
         .pipe(less())
         .pipe(gulp.dest('bin/Debug/css'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('templates/*.pug', ['html']);
+    gulp.watch('styles/*.less', ['css']);
 });
 
 gulp.task('default', ['html', 'css']);
