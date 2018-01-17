b0ea215a482ba5ca5694d2b14f3591868cfe9073
Nikolaos Georgiou <Nikolaos.Georgiou@gmail.com>
2018-01-17 15:47:08 +0100

    Added vue linting.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,33 +1,22 @@
 <template>
-  <div id="app">
-    <img src="http://vuejs.org/images/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vueify" target="_blank">vueify</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div id="app">
+        <img src="http://vuejs.org/images/logo.png">
+        <h1>{{ msg }}</h1>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    name: 'App',
+
+    /**
+     * Returns the initial data.
+     * @returns {object} The initial data.
+     */
+    data() {
+        return { msg: 'Welcome to Your Vue.js App' };
     }
-  }
-}
+};
 </script>
 
 <style scoped>
