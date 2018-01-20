d63af86e7b15d043475846a2cf968c7bfba9f201
Nikolaos Georgiou <Nikolaos.Georgiou@gmail.com>
2018-01-20 13:54:48 +0100

    Testing Travis with dot net core

---
+++ server/BuzzStats.ListIngester/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BuzzStats.ListIngester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
