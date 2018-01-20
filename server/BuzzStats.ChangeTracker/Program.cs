49cea9f1b15f8ab9862515fa95d6bd05fc4cfe36
Nikolaos Georgiou <Nikolaos.Georgiou@gmail.com>
2018-01-20 17:00:59 +0100

    Added empty projects. (#11)
    * Adding new empty projects.
    * unit testing all projects in travis.

---
+++ server/BuzzStats.ChangeTracker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BuzzStats.ChangeTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ server/BuzzStats.StoryUpdater/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BuzzStats.StoryUpdater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
