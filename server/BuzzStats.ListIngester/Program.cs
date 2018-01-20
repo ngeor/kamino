5bfbbc19681cfa07dae619cd393a7ab38c3a5cf8
Nikolaos Georgiou <Nikolaos.Georgiou@gmail.com>
2018-01-20 14:43:48 +0100

    trying to build with confluent kafka package

--- server/BuzzStats.ListIngester/Program.cs
+++ server/BuzzStats.ListIngester/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace BuzzStats.ListIngester
 {
@@ -7,6 +12,36 @@ namespace BuzzStats.ListIngester
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string brokerList = args[0];
+            var topics = args.Skip(1).ToList();
+
+            var config = new Dictionary<string, object>
+            {
+                { "group.id", "simple-csharp-consumer" },
+                { "bootstrap.servers", brokerList }
+            };
+
+            using (var consumer = new Consumer<Ignore, string>(config, null, new StringDeserializer(Encoding.UTF8)))
+            {
+                consumer.Assign(new List<TopicPartitionOffset> { new TopicPartitionOffset(topics.First(), 0, 0) });
+
+                // Raised on critical errors, e.g. connection failures or all brokers down.
+                consumer.OnError += (_, error)
+                    => Console.WriteLine($"Error: {error}");
+
+                // Raised on deserialization errors or when a consumed message has an error != NoError.
+                consumer.OnConsumeError += (_, error)
+                    => Console.WriteLine($"Consume error: {error}");
+
+                while (true)
+                {
+                    Message<Ignore, string> msg;
+                    if (consumer.Consume(out msg, TimeSpan.FromSeconds(1)))
+                    {
+                        Console.WriteLine($"Topic: {msg.Topic} Partition: {msg.Partition} Offset: {msg.Offset} {msg.Value}");
+                    }
+                }
+            }
         }
     }
 }
