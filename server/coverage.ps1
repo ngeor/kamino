0385bef300dfb60e0137ff24d4977dc151670ff3
Nikolaos Georgiou <Nikolaos.Georgiou@gmail.com>
2018-01-27 13:05:10 +0100

    Enabling code coverage.

--- server/coverage.ps1
+++ server/coverage.ps1
@@ -1,11 +1,17 @@
+$UnitTestDirs = Get-ChildItem . | Where-Object { $_.PSIsContainer -and $_.Name.EndsWith("UnitTests")}
+Remove-Item -Force .\opencover.xml
 nuget install OpenCover -Version 4.6.519 -OutputDirectory packages
 nuget install ReportGenerator -Version 2.5.9 -OutputDirectory packages
-nuget install NUnit.ConsoleRunner -Version 3.7.0 -OutputDirectory packages
 
-.\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe `
-	-register:user `
-	-filter:"+[Buzz*]* -[*.UnitTests]*" `
-	-target:.\packages\NUnit.ConsoleRunner.3.7.0\tools\nunit3-console.exe `
-	-targetargs:".\BuzzStats.WebApi.UnitTests\bin\Debug\BuzzStats.WebApi.UnitTests.dll"
+foreach ($dir in $UnitTestDirs) {
+	.\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe `
+		-oldstyle `
+		-output:opencover.xml `
+		-mergeoutput `
+		-register:user `
+		-filter:"+[Buzz*]* -[*.UnitTests]*" `
+		-target:"C:\Program Files\dotnet\dotnet.exe" `
+		-targetargs:"test --no-build $dir" `
+}
 
-.\packages\ReportGenerator.2.5.9\tools\ReportGenerator.exe -reports:results.xml -targetdir:coveragereport
+.\packages\ReportGenerator.2.5.9\tools\ReportGenerator.exe -reports:opencover.xml -targetdir:coverage
---
+++ server/sonar.ps1
@@ -0,0 +1,15 @@
+if (-Not ($Env:SONAR_LOGIN)) {
+    Write-Error -Category InvalidArgument -Message "Please set the SONAR_LOGIN environment variable"
+    Exit 1
+}
+
+SonarQube.Scanner.MSBuild.exe begin /k:"BuzzStats" `
+    /d:sonar.organization="ngeor-github" `
+    /d:sonar.host.url="https://sonarcloud.io" `
+    /d:sonar.login="$Env:SONAR_LOGIN" `
+    /d:sonar.cs.opencover.reportsPaths="opencover.xml"
+
+MSBuild.exe /t:Rebuild .\BuzzStats.sln
+.\coverage.ps1
+
+SonarQube.Scanner.MSBuild.exe end /d:sonar.login="$Env:SONAR_LOGIN"
