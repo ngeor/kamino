8100e91f0db55665c94259f8571fb3d0a44c4cc0
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2020-10-18 09:48:40 +0200

    Merge commit 'd32892179f141a9ec7785f79953d37614b7f3c21' as 'checkstyle-rules'

---
+++ src/test/resources/rules/ImportOrder1.java
@@ -0,0 +1,16 @@
+package rules;
+
+import com.foo.Bar;
+import java.io.File;
+import org.something.Else;
+
+import static com.foo.Bar.something;
+import static java.io.File.somethingElse;
+import static org.something.Else.anotherOne;
+
+/**
+ * Showing import order.
+ */
+class ImportOrder1 {
+
+}
