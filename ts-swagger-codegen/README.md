409313332553acdc4741f4ed008b8b0838dbe6df
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2021-05-29 07:39:26 +0200

    Removed ts-swagger-codegen
    ts-swagger-codegen was supposed to be a swagger code generator
    written in TypeScript. The idea was abandoned early on.

--- ts-swagger-codegen/README.md
+++
@@ -1,4 +0,0 @@
-# ts-swagger-codegen
-Swagger code generator (in TypeScript)
-
-[![Build Status](https://travis-ci.org/ngeor/ts-swagger-codegen.svg?branch=master)](https://travis-ci.org/ngeor/ts-swagger-codegen)
--- ts-swagger-codegen/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  clearMocks: true,
-  // enabling coverage confuses debugging
-  // collectCoverage: true
-  coverageDirectory: "coverage",
-  preset: "ts-jest",
-  testEnvironment: "node"
-};
