d609639f57d9f7363328d2ea89b4c068c90c9bae
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2021-09-27 08:38:48 +0200

    [yak4j-cli] Wrapper script runs jar file from temp folder
    To be able to use yak4j-cli to release itself (which requires cleaning the target folder), copying the jar file into a temp location and running it from there.

--- scripts/yak4j-cli.sh
+++ scripts/yak4j-cli.sh
@@ -1,5 +1,15 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 # This script is automatically generated, do not edit manually.
 # Re-generate the script with mvn package -pl apps/yak4j-cli -am
 set -e
-java -jar apps/yak4j-cli/target/yak4j-cli-0.10.0-SNAPSHOT.jar $*
+JAR_NAME=yak4j-cli-0.10.0-SNAPSHOT.jar
+SOURCE_JAR=apps/yak4j-cli/target/${JAR_NAME}
+if [[ ! -r ${SOURCE_JAR} ]]; then
+    mvn package -pl apps/yak4j-cli -am
+fi
+# Use jar from a temp location, to be able to clean the folder of yak4j-cli
+TEMP_JAR=${TMP}/${JAR_NAME}
+# Delete the temp file upon exit
+trap "{ rm ${TEMP_JAR}; }" EXIT
+cp ${SOURCE_JAR} ${TEMP_JAR}
+java -jar ${TEMP_JAR} $*
