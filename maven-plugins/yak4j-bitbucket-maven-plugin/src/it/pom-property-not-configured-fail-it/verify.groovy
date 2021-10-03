7ea121704d03f6487f56e745ce71f642a5bcfee7
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2021-10-03 11:19:30 +0200

    Migrated 'maven-plugins/yak4j-bitbucket-maven-plugin' to new repo 'https://github.com/ngeor/yak4j-bitbucket-maven-plugin'

--- maven-plugins/yak4j-bitbucket-maven-plugin/src/it/pom-property-not-configured-fail-it/verify.groovy
+++
@@ -1,9 +0,0 @@
-import java.nio.file.Files
-import java.nio.charset.StandardCharsets
-
-def logFile = new File(basedir, "build.log")
-assert logFile.isFile()
-
-def contents = new String(Files.readAllBytes(logFile.toPath()), StandardCharsets.UTF_8)
-assert !contents.isEmpty()
-assert contents.contains("The parameters 'propertyName'") // is missing or invalid
