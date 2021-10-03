c4343853d52999c474fc27d94b69bc158ee63789
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2021-10-03 11:36:16 +0200

    Migrated 'apps/bitbucket-pr-report' to new repo 'https://github.com/ngeor/bitbucket-pr-report'

--- apps/bitbucket-pr-report/web2/src/main/resources/db/migration/R__2019.07.27.18.00_credentials.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS credentials (
-  owner varchar(100) not null,
-  username varchar(100) not null,
-  password varchar(100) not null,
-  primary key (owner)
-);
-
-ALTER TABLE repository ADD COLUMN owner varchar(100) NOT NULL;
-CREATE INDEX owner_idx ON repository(owner);
--- apps/bitbucket-pr-report/web2/src/main/resources/db/migration/R__2019.07.28.08.46_username.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE user DROP COLUMN username;
-ALTER TABLE user ADD COLUMN display_name varchar(100) NOT NULL UNIQUE;
