20782763ad00070379a0fe7bce1ec3e50183c9b0
Nikolaos Georgiou <Nikolaos.Georgiou@gmail.com>
2023-02-14 21:21:36 +0100

    chore!: Do not follow semver anymore (#42)
    * chore!: Do not follow semver anymore
    
    - Instead of releasing on tag, releasing from the main branch.
    - Automatically appending the GitHub Action Run Number to the version, not following SemVer anymore.

--- scripts/release.py
+++ scripts/release.py
@@ -28,13 +28,18 @@ def parse_args():
         help="The password to use for publishing the release to Maven",
         required=True,
     )
+    parser.add_argument(
+        "--revision",
+        help="The build number, will be appended to the version",
+        required=True,
+    )
     return parser.parse_args()
 
 
 def release(args):
     try:
         perform_release(
-            args.gpg_key, args.gpg_passphrase, args.maven_username, args.maven_password
+            args.gpg_key, args.gpg_passphrase, args.maven_username, args.maven_password, args.revision
         )
     finally:
         clean_gpg(args.gpg_key)
@@ -56,7 +61,7 @@ def clean_gpg(gpg_key):
     subprocess.run(["gpg", "--batch", "--yes", "--delete-key", gpg_key], check=True)
 
 
-def perform_release(gpg_key, gpg_passphrase, maven_username, maven_password):
+def perform_release(gpg_key, gpg_passphrase, maven_username, maven_password, revision):
     with tempfile.TemporaryDirectory() as tmp_dir:
         settings_xml_file = os.path.join(tmp_dir, "settings.xml")
         with open(settings_xml_file, "w") as f:
@@ -92,6 +97,7 @@ def perform_release(gpg_key, gpg_passphrase, maven_username, maven_password):
                 "-Dcheckstyle.skip=true",
                 "-Djacoco.skip=true",
                 "-Dinvoker.skip=true",
+                f"-Drevision={revision}"
                 "-Pgpg",
                 "deploy",
             ],
