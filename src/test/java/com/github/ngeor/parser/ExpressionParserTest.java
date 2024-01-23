5d67ecbcd373f088ee53cc29c79533831470c199
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2024-01-23 20:52:00 +0100

    refactor ExpressionParserTest

--- src/test/java/com/github/ngeor/parser/ExpressionParserTest.java
+++ src/test/java/com/github/ngeor/parser/ExpressionParserTest.java
@@ -6,25 +6,32 @@ import org.junit.jupiter.api.Test;
 
 @SuppressWarnings("MagicNumber")
 class ExpressionParserTest {
+    private String input;
+    private ParseResult<Expression> parseResult;
+
     @Test
     void literalDigit() {
-        Tokenizer tokenizer = new Tokenizer("42");
-        Parser<Expression> parser = new ExpressionParser();
-        assertThat(parser.parse(tokenizer).value()).isEqualTo(new Expression.LiteralDigit("42"));
+        input = "42";
+        act();
+        assertThat(parseResult.value()).isEqualTo(new Expression.LiteralDigit("42"));
     }
 
     @Test
     void name() {
-        Tokenizer tokenizer = new Tokenizer("Answer");
-        Parser<Expression> parser = new ExpressionParser();
-        assertThat(parser.parse(tokenizer).value()).isEqualTo(new Expression.Name("Answer"));
+        input = "Answer";
+        act();
+        assertThat(parseResult.value()).isEqualTo(new Expression.Name("Answer"));
     }
 
     @Test
     void minusLiteralDigit() {
-        Tokenizer tokenizer = new Tokenizer("-42");
-        Parser<Expression> parser = new ExpressionParser();
-        assertThat(parser.parse(tokenizer).value())
+        input = "-42";
+        act();
+        assertThat(parseResult.value())
                 .isEqualTo(new Expression.UnaryExpression("-", new Expression.LiteralDigit("42")));
+    }
+
+    private void act() {
+        parseResult = new ExpressionParser().parse(new Tokenizer(input));
     }
 }
