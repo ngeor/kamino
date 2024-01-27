13ef3563e160c6c7bce72f833046604095365036
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2024-01-27 11:22:53 +0100

    Scaffolding template manager

---
+++ internal-tooling/sifo/src/main/java/com/github/ngeor/App.java
@@ -0,0 +1,15 @@
+package com.github.ngeor;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {}
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+    }
+}
---
+++ internal-tooling/sifo/src/test/java/com/github/ngeor/AppTest.java
@@ -0,0 +1,18 @@
+package com.github.ngeor;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testApp() {
+        assertThat(true).isTrue();
+    }
+}
