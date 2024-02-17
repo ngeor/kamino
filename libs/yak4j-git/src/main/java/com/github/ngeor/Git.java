6154119f1aea62a2373954564004736d60d412b9
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2024-02-17 09:30:51 +0100

    feat: Added getMostRecentTagWithDate

--- libs/yak4j-git/src/main/java/com/github/ngeor/Git.java
+++ libs/yak4j-git/src/main/java/com/github/ngeor/Git.java
@@ -2,8 +2,12 @@ package com.github.ngeor;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -14,13 +18,13 @@ public final class Git {
         this.processHelper = new ProcessHelper(workingDirectory, "git");
     }
 
-    public String getDefaultBranch() throws IOException, InterruptedException {
+    public String getDefaultBranch() throws IOException, InterruptedException, ProcessFailedException {
         String remote = getOnlyRemote();
         String output = processHelper.run("symbolic-ref", "refs/remotes/" + remote + "/HEAD", "--short");
         return output.replace(remote + "/", "").trim();
     }
 
-    public String getOnlyRemote() throws IOException, InterruptedException {
+    public String getOnlyRemote() throws IOException, InterruptedException, ProcessFailedException {
         List<String> remotes = getRemotes();
         if (remotes.size() != 1) {
             throw new IllegalStateException("Multiple remotes found");
@@ -29,32 +33,32 @@ public final class Git {
         return remotes.get(0);
     }
 
-    public List<String> getRemotes() throws IOException, InterruptedException {
+    public List<String> getRemotes() throws IOException, InterruptedException, ProcessFailedException {
         return processHelper.run("remote").lines().collect(Collectors.toList());
     }
 
-    public void checkout(String branch) throws IOException, InterruptedException {
+    public void checkout(String branch) throws IOException, InterruptedException, ProcessFailedException {
         processHelper.run("checkout", branch);
     }
 
-    public void pull() throws IOException, InterruptedException {
+    public void pull() throws IOException, InterruptedException, ProcessFailedException {
         processHelper.run("pull");
     }
 
     public void subTreeAdd(String destination, File oldRepoRoot, String oldRepoBranch)
-            throws IOException, InterruptedException {
+            throws IOException, InterruptedException, ProcessFailedException {
         processHelper.run("subtree", "add", "-P", destination, oldRepoRoot.getAbsolutePath(), oldRepoBranch);
     }
 
-    public void addAll() throws IOException, InterruptedException {
+    public void addAll() throws IOException, InterruptedException, ProcessFailedException {
         processHelper.run("add", "-A");
     }
 
-    public void commit(String message) throws IOException, InterruptedException {
+    public void commit(String message) throws IOException, InterruptedException, ProcessFailedException {
         processHelper.run("commit", "-m", message);
     }
 
-    public void push() throws IOException, InterruptedException {
+    public void push() throws IOException, InterruptedException, ProcessFailedException {
         processHelper.run("push");
     }
 
@@ -64,16 +68,29 @@ public final class Git {
      * @param prefix The prefix of the tag.
      * @return The most recent tag, stripped of the given prefix.
      */
-    public String getMostRecentTag(String prefix) throws IOException, InterruptedException {
-        String output = processHelper.run("tag", "-l", prefix + "*", "--sort=-version:refname");
-        return output.lines()
-                .map(s -> s.substring(prefix.length()))
-                .findFirst()
-                .orElseThrow(() -> new IllegalStateException(
-                        String.format("Could not find any git tags starting with %s", prefix)));
+    public Optional<String> getMostRecentTag(String prefix)
+            throws IOException, InterruptedException, ProcessFailedException {
+        return getMostRecentTag(prefix, null, s -> s.substring(prefix.length()));
     }
 
-    public void add(String file) throws IOException, InterruptedException {
+    public Optional<String[]> getMostRecentTagWithDate(String prefix)
+            throws IOException, InterruptedException, ProcessFailedException {
+        return getMostRecentTag(
+                prefix, "--format=\"%(refname:strip=2),%(creatordate)\"", s -> s.substring(prefix.length())
+                        .split(","));
+    }
+
+    private <E> Optional<E> getMostRecentTag(String prefix, String format, Function<String, E> mapper)
+            throws IOException, InterruptedException, ProcessFailedException {
+        List<String> args = new ArrayList<>(Arrays.asList("tag", "-l", prefix + "*", "--sort=-version:refname"));
+        if (format != null) {
+            args.add(format);
+        }
+        String output = processHelper.run(args.toArray(String[]::new));
+        return output.lines().map(mapper).findFirst();
+    }
+
+    public void add(String file) throws IOException, InterruptedException, ProcessFailedException {
         processHelper.run("add", file);
     }
 
@@ -90,12 +107,22 @@ public final class Git {
      * {@code SHA|yyyy-MM-dd|object|summary}
      * where "object" can be in the form {@code tag: tagname} for tags.
      */
-    public Stream<Commit> revList(String commit, String path) throws IOException, InterruptedException {
+    public Stream<Commit> revList(String since, String path)
+            throws IOException, InterruptedException, ProcessFailedException {
+        String commit = since == null ? null : since + "..HEAD";
         return processHelper
                 .run(Stream.of("rev-list", "--all", "--format=%H|%as|%D|%s", commit, path)
                         .filter(Objects::nonNull)
                         .toArray(String[]::new))
                 .lines()
                 .flatMap(line -> Commit.parse(line).stream());
+    }
+
+    public void init() throws IOException, InterruptedException, ProcessFailedException {
+        processHelper.run("init");
+    }
+
+    public void tag(String tag) throws IOException, ProcessFailedException, InterruptedException {
+        processHelper.run("tag", tag);
     }
 }
---
+++ libs/yak4j-git/src/test/java/com/github/ngeor/GitIT.java
@@ -0,0 +1,100 @@
+package com.github.ngeor;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.within;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Locale;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GitIT {
+    private File directory;
+    private Git git;
+
+    @BeforeEach
+    void beforeEach() throws IOException {
+        directory = Files.createTempDirectory("git").toFile();
+        git = new Git(directory);
+    }
+
+    @AfterEach
+    void afterEach() throws IOException {
+        FileUtils.deleteDirectory(directory);
+    }
+
+    @Test
+    void testHasStagedChanges() throws IOException, InterruptedException, ProcessFailedException {
+        git.init();
+        assertThat(git.hasStagedChanges()).isFalse();
+        Files.writeString(directory.toPath().resolve("README.md"), "My project");
+        git.addAll();
+        assertThat(git.hasStagedChanges()).isTrue();
+        git.commit("Adding readme file");
+        assertThat(git.hasStagedChanges()).isFalse();
+    }
+
+    @Test
+    void testGetMostRecentTag() throws IOException, ProcessFailedException, InterruptedException {
+        git.init();
+
+        Files.writeString(directory.toPath().resolve("README.md"), "My project");
+        git.addAll();
+        git.commit("Adding readme file");
+        git.tag("v1.0.0");
+
+        Files.writeString(directory.toPath().resolve("CHANGELOG.md"), "My changelog");
+        git.addAll();
+        git.commit("Adding changelog file");
+        git.tag("v1.1.0");
+
+        assertThat(git.getMostRecentTag("v")).contains("1.1.0");
+    }
+
+    @Test
+    void testGetMostRecentTagWithDate() throws IOException, ProcessFailedException, InterruptedException {
+        git.init();
+
+        Files.writeString(directory.toPath().resolve("README.md"), "My project");
+        git.addAll();
+        git.commit("Adding readme file");
+        git.tag("v9.0.0");
+
+        Files.writeString(directory.toPath().resolve("CHANGELOG.md"), "My changelog");
+        git.addAll();
+        git.commit("Adding changelog file");
+        git.tag("v10.0.0");
+
+        String[] parts = git.getMostRecentTagWithDate("v").orElseThrow();
+
+        assertThat(parts[0]).isEqualTo("10.0.0");
+        assertThat(OffsetDateTime.parse(parts[1], DateTimeFormatter.ofPattern("E MMM d HH:mm:ss yyyy Z", Locale.US)))
+                .isCloseToUtcNow(within(2, ChronoUnit.SECONDS));
+    }
+
+    @Test
+    void testGetRevList() throws IOException, ProcessFailedException, InterruptedException {
+        git.init();
+
+        Files.writeString(directory.toPath().resolve("README.md"), "My project");
+        git.addAll();
+        git.commit("Adding readme file");
+        git.tag("v9.0.0");
+
+        Files.writeString(directory.toPath().resolve("CHANGELOG.md"), "My changelog");
+        git.addAll();
+        git.commit("Adding changelog file");
+        git.tag("v10.0.0");
+
+        assertThat(git.revList(null, ".").map(Commit::summary))
+                .containsExactly("Adding changelog file", "Adding readme file");
+        assertThat(git.revList("v9.0.0", ".").map(Commit::summary)).containsExactly("Adding changelog file");
+    }
+}
