6a71e9828bee6e9f7460f5f01cfed42a208f3010
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2024-02-18 15:00:19 +0100

    chore: spotless

--- libs/yak4j-process/src/main/java/com/github/ngeor/ProcessHelper.java
+++ libs/yak4j-process/src/main/java/com/github/ngeor/ProcessHelper.java
@@ -26,15 +26,15 @@ public class ProcessHelper {
 
     private String doRun(List<String> command) throws IOException, InterruptedException, ProcessFailedException {
         Process process = new ProcessBuilder(command)
-            .directory(workingDirectory)
-            .redirectErrorStream(true)
-            .start();
+                .directory(workingDirectory)
+                .redirectErrorStream(true)
+                .start();
         String output = new String(process.getInputStream().readAllBytes());
         int exitCode = process.waitFor();
         if (exitCode != 0) {
             String commandAsString = String.join(" ", command);
             throw new ProcessFailedException(
-                String.format("Error running %s in %s: %s", commandAsString, workingDirectory, output));
+                    String.format("Error running %s in %s: %s", commandAsString, workingDirectory, output));
         }
         return output;
     }
@@ -49,14 +49,14 @@ public class ProcessHelper {
 
     private void doRunInheritIO(List<String> command) throws IOException, InterruptedException, ProcessFailedException {
         Process process = new ProcessBuilder(command)
-            .directory(workingDirectory)
-            .inheritIO()
-            .start();
+                .directory(workingDirectory)
+                .inheritIO()
+                .start();
         int exitCode = process.waitFor();
         if (exitCode != 0) {
             String commandAsString = String.join(" ", command);
             throw new ProcessFailedException(
-                String.format("Error running %s in %s", commandAsString, workingDirectory));
+                    String.format("Error running %s in %s", commandAsString, workingDirectory));
         }
     }
 
@@ -70,9 +70,9 @@ public class ProcessHelper {
 
     private boolean doTryRun(List<String> command) throws IOException, InterruptedException {
         Process process = new ProcessBuilder(command)
-            .directory(workingDirectory)
-            .redirectErrorStream(true)
-            .start();
+                .directory(workingDirectory)
+                .redirectErrorStream(true)
+                .start();
         process.getInputStream().readAllBytes();
         return process.waitFor() == 0;
     }
