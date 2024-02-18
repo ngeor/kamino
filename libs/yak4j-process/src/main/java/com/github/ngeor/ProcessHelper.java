ed3d956ac130f86d9a0b71936fd6d368fe3393aa
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2024-02-18 14:38:39 +0100

    feat: Offer overload methods that accept arguments as collection

--- libs/yak4j-process/src/main/java/com/github/ngeor/ProcessHelper.java
+++ libs/yak4j-process/src/main/java/com/github/ngeor/ProcessHelper.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 public class ProcessHelper {
@@ -15,51 +16,75 @@ public class ProcessHelper {
         this.baseCommand = Arrays.asList(baseCommand);
     }
 
-    // TODO micro-optimisation: do not return the entire String but a stream (with a completed future approach)
     public String run(String... args) throws IOException, InterruptedException, ProcessFailedException {
-        List<String> command = createArgs(args);
+        return doRun(createArgs(args));
+    }
+
+    public String run(Collection<String> args) throws IOException, InterruptedException, ProcessFailedException {
+        return doRun(createArgs(args));
+    }
+
+    private String doRun(List<String> command) throws IOException, InterruptedException, ProcessFailedException {
         Process process = new ProcessBuilder(command)
-                .directory(workingDirectory)
-                .redirectErrorStream(true)
-                .start();
+            .directory(workingDirectory)
+            .redirectErrorStream(true)
+            .start();
         String output = new String(process.getInputStream().readAllBytes());
         int exitCode = process.waitFor();
         if (exitCode != 0) {
             String commandAsString = String.join(" ", command);
             throw new ProcessFailedException(
-                    String.format("Error running %s in %s: %s", commandAsString, workingDirectory, output));
+                String.format("Error running %s in %s: %s", commandAsString, workingDirectory, output));
         }
         return output;
     }
 
     public void runInheritIO(String... args) throws IOException, InterruptedException, ProcessFailedException {
-        List<String> command = createArgs(args);
+        doRunInheritIO(createArgs(args));
+    }
+
+    public void runInheritIO(Collection<String> args) throws IOException, InterruptedException, ProcessFailedException {
+        doRunInheritIO(createArgs(args));
+    }
+
+    private void doRunInheritIO(List<String> command) throws IOException, InterruptedException, ProcessFailedException {
         Process process = new ProcessBuilder(command)
-                .directory(workingDirectory)
-                .inheritIO()
-                .start();
+            .directory(workingDirectory)
+            .inheritIO()
+            .start();
         int exitCode = process.waitFor();
         if (exitCode != 0) {
             String commandAsString = String.join(" ", command);
             throw new ProcessFailedException(
-                    String.format("Error running %s in %s", commandAsString, workingDirectory));
+                String.format("Error running %s in %s", commandAsString, workingDirectory));
         }
     }
 
     public boolean tryRun(String... args) throws IOException, InterruptedException {
-        List<String> command = createArgs(args);
+        return doTryRun(createArgs(args));
+    }
+
+    public boolean tryRun(Collection<String> args) throws IOException, InterruptedException {
+        return doTryRun(createArgs(args));
+    }
+
+    private boolean doTryRun(List<String> command) throws IOException, InterruptedException {
         Process process = new ProcessBuilder(command)
-                .directory(workingDirectory)
-                .redirectErrorStream(true)
-                .start();
+            .directory(workingDirectory)
+            .redirectErrorStream(true)
+            .start();
         process.getInputStream().readAllBytes();
         return process.waitFor() == 0;
     }
 
     private List<String> createArgs(String... args) {
-        List<String> result = new ArrayList<>(baseCommand.size() + args.length);
+        return createArgs(Arrays.asList(args));
+    }
+
+    private List<String> createArgs(Collection<String> args) {
+        List<String> result = new ArrayList<>(baseCommand.size() + args.size());
         result.addAll(baseCommand);
-        result.addAll(Arrays.asList(args));
+        result.addAll(args);
         return result;
     }
 }
