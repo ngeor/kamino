7cb20609ab37972aa5cb03bd1f31e466bc370150
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2024-02-20 13:35:33 +0100

    fix: Support tags at HEAD

--- libs/yak4j-git/src/main/java/com/github/ngeor/Commit.java
+++ libs/yak4j-git/src/main/java/com/github/ngeor/Commit.java
@@ -3,32 +3,31 @@ package com.github.ngeor;
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public record Commit(String sha, LocalDate authorDate, String tag, String summary) {
+    private static final Pattern pattern = Pattern.compile(
+            "^(?<sha>[0-9a-f]+)\\|(?<date>\\d{4}-\\d{2}-\\d{2})\\|.*?(tag:\\s*(?<tag>.+))?\\|(?<summary>.+)$");
+
     public static Optional<Commit> parse(String line) {
         if (line == null) {
             return Optional.empty();
         }
 
-        String[] parts = line.split("\\|", 4);
-        if (parts.length != 4) {
+        Matcher matcher = pattern.matcher(line);
+        if (!matcher.matches()) {
             return Optional.empty();
         }
 
         LocalDate authorDate;
         try {
-            authorDate = LocalDate.parse(parts[1]);
+            authorDate = LocalDate.parse(matcher.group("date"));
         } catch (DateTimeParseException ignored) {
             return Optional.empty();
         }
 
-        String tag;
-        if (parts[2].startsWith("tag: ")) {
-            tag = parts[2].substring("tag: ".length());
-        } else {
-            tag = null;
-        }
-
-        return Optional.of(new Commit(parts[0], authorDate, tag, parts[3]));
+        return Optional.of(
+                new Commit(matcher.group("sha"), authorDate, matcher.group("tag"), matcher.group("summary")));
     }
 }
--- libs/yak4j-git/src/test/java/com/github/ngeor/CommitTest.java
+++ libs/yak4j-git/src/test/java/com/github/ngeor/CommitTest.java
@@ -9,13 +9,26 @@ import org.junit.jupiter.api.Test;
 class CommitTest {
     @Test
     void parseWithoutTag() {
-        Optional<Commit> result = Commit.parse("sha|2024-02-11||chore: Whatever");
-        assertThat(result).contains(new Commit("sha", LocalDate.parse("2024-02-11"), null, "chore: Whatever"));
+        Optional<Commit> result = Commit.parse("f42|2024-02-11||chore: Whatever");
+        assertThat(result).contains(new Commit("f42", LocalDate.parse("2024-02-11"), null, "chore: Whatever"));
     }
 
     @Test
     void parseWithTag() {
-        Optional<Commit> result = Commit.parse("sha2|2024-02-10|tag: v5.1.0|fix: Whatever");
-        assertThat(result).contains(new Commit("sha2", LocalDate.parse("2024-02-10"), "v5.1.0", "fix: Whatever"));
+        Optional<Commit> result = Commit.parse("b31|2024-02-10|tag: v5.1.0|fix: Whatever");
+        assertThat(result).contains(new Commit("b31", LocalDate.parse("2024-02-10"), "v5.1.0", "fix: Whatever"));
+    }
+
+    @Test
+    void parseWithTagAtHead() {
+        // 30efa45c842d444b49ceb49a03e22e2bbdf600de|2024-02-20|HEAD -> main, tag: v1.0|deps: Upgraded mockito
+        Optional<Commit> result = Commit.parse(
+                "30efa45c842d444b49ceb49a03e22e2bbdf600de|2024-02-20|HEAD -> main, tag: v1.0|deps: Upgraded mockito");
+        assertThat(result)
+                .contains(new Commit(
+                        "30efa45c842d444b49ceb49a03e22e2bbdf600de",
+                        LocalDate.parse("2024-02-20"),
+                        "v1.0",
+                        "deps: Upgraded mockito"));
     }
 }
