63f18a964655f47d690f9d83331f0a8f0d15adf7
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2024-02-23 20:40:52 +0100

    feat: Added more options to build.sh script

--- build.sh
+++ build.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
 set -e
+
+# Collect options
+BUILD_NATIVE=0
+RUN_REWRITE=0
+SKIP_TESTS=0
+while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do case $1 in
+    -n | --native )
+        BUILD_NATIVE=1
+        ;;
+    -r | --rewrite )
+        RUN_REWRITE=1
+        ;;
+    -s | --skip-tests )
+        SKIP_TESTS=1
+        ;;
+esac; shift; done
+if [[ "$1" == '--' ]]; then shift; fi
+
 # sort pom
 mvn -Psortpom validate
 # sort root pom file (does not currently have a sortpom configuration)
@@ -7,13 +25,21 @@ mvn -N com.github.ekryd.sortpom:sortpom-maven-plugin:sort -Dsort.sortModules=tru
 # remove pom.bak files etc
 git clean -f -X
 # build
-mvn clean test
-# spotless (allowed to fail for now)
-mvn spotless:apply || true
+if [[ $SKIP_TESTS -eq 1 ]]; then
+    mvn clean test -DskipTests
+else
+    mvn clean test
+fi
+# spotless (excluding the aggregator root)
+mvn -pl '!:kamino' spotless:apply
+if [[ $RUN_REWRITE -eq 1 ]]; then
+    # open rewrite (excluding the aggregator root)
+    mvn -pl '!:kamino' rewrite:run
+fi
 # build internal tools
 mvn -Pshade -am -pl internal-tooling/changes package -DskipTests
 mvn -Pshade -am -pl internal-tooling/sifo package -DskipTests
-if [[ "$1" == "--native" ]]; then
+if [[ $BUILD_NATIVE -eq 1 ]]; then
     mvn -Pnative -am -pl internal-tooling/changes package -DskipTests
     mvn -Pnative -am -pl internal-tooling/sifo package -DskipTests
 fi
