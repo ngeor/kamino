f00bda022ddc34454574099668d53f4a96426195
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2024-02-27 21:27:19 +0100

    feat: Added "modules" field in known element names

--- libs/yak4j-maven/src/main/java/com/github/ngeor/maven/ElementNames.java
+++ libs/yak4j-maven/src/main/java/com/github/ngeor/maven/ElementNames.java
@@ -5,6 +5,7 @@ public final class ElementNames {
 
     public static final String ARTIFACT_ID = "artifactId";
     public static final String GROUP_ID = "groupId";
+    public static final String MODULES = "modules";
     public static final String PARENT = "parent";
     public static final String PROJECT = "project";
     public static final String RELATIVE_PATH = "relativePath";
