be925ff639656c8d7407ac0f5f96e832ebb1b6c8
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2024-03-17 19:47:41 +0100

    deleted old apis

--- libs/yak4j-maven/src/main/java/com/github/ngeor/maven/document/MergerNg.java
+++
@@ -1,7 +0,0 @@
-package com.github.ngeor.maven.document;
-
-public interface MergerNg {
-    EffectivePomDocument merge(PomDocument root);
-
-    EffectivePomDocument merge(EffectivePomDocument left, PomDocument right);
-}
--- libs/yak4j-maven/src/main/java/com/github/ngeor/maven/document/ParentFinderNg.java
+++
@@ -1,8 +0,0 @@
-package com.github.ngeor.maven.document;
-
-import java.util.Optional;
-
-@FunctionalInterface
-public interface ParentFinderNg<E extends BasePomDocument> {
-    Optional<E> findParent(E pomDocument);
-}
