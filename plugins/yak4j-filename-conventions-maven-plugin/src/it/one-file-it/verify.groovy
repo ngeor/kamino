6e5be8c17c40479206ed1135b2a2df26bbc533c1
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2025-02-06 13:15:56 +0100

    chore: Moved yak4j-filename-conventions-maven-plugin back to its own repo

--- plugins/yak4j-filename-conventions-maven-plugin/src/it/one-file-it/verify.groovy
+++
@@ -1,9 +0,0 @@
-import java.nio.charset.StandardCharsets
-import java.nio.file.Files
-
-def logFile = new File(basedir, "build.log")
-assert logFile.isFile()
-
-def contents = new String(Files.readAllBytes(logFile.toPath()), StandardCharsets.UTF_8)
-assert !contents.isEmpty()
-assert contents.contains("1 file(s) in directory")
